87acb14d9a8b3d98af7431874537b0f72c217358
Matthew Weier O'Phinney <matthew@zend.com>
2018-02-27 09:18:49 -0600

    Remove unused import

--- test/OAuth2AdapterFactoryTest.php
+++ test/OAuth2AdapterFactoryTest.php
@@ -10,7 +10,6 @@ declare(strict_types=1);
 
 namespace ZendTest\Expressive\Authentication\OAuth2;
 
-use function foo\func;
 use League\OAuth2\Server\ResourceServer;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
